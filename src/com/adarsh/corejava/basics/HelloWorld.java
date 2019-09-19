ee9b8136e6766062a410b4b46da22eaade1a9034
adarshhs <adarsh.hs.mails@gmail.com>
2019-09-19 16:19:13 +0530

    Update HelloWorld.java

--- src/com/adarsh/corejava/basics/HelloWorld.java
+++ src/com/adarsh/corejava/basics/HelloWorld.java
@@ -2,9 +2,9 @@ package com.adarsh.corejava.basics;
 
 public class HelloWorld
 {
-
 	public static void main(String[] args) 
 	{
+		//prints hello world on console
 		System.out.println("Hello world");
 	}
 }
