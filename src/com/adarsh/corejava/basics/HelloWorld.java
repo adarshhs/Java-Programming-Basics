b6595f1054ddd1e9255b3de6428aabbf54be7f93
adarshhs <adarshhedagarahallishivappa@gmail.com>
2025-03-29 22:02:48 +0530

    setting up ide

--- src/com/adarsh/corejava/basics/HelloWorld.java
+++ src/com/adarsh/corejava/basics/HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld
 	public static void main(String[] args) 
 	{
 		//prints hello world on console
-		System.out.println("Hello world");
+		System.out.println("Hello world!!");
 	}
 }
